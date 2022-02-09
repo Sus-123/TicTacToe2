ac6fc8331aec9402ff770c91bb47bc02cb94f6e6
sushant singh <iit2018171@iiita.ac.in>
2022-02-10 00:33:44 +0530

    Initial commit

---
+++ src/com/company/TicTac.java
@@ -0,0 +1,61 @@
+package com.company;
+import java.util.Scanner;
+
+public class TicTac {
+    private boolean player1 = true;
+    private boolean player2 = true;
+    private  boolean gameover = false ;
+    private final int size ;
+    Scanner sc = new Scanner(System.in);
+
+    TicTac(){
+        System.out.println("Enter size of board : ");
+        size = sc.nextInt();
+    }
+
+    public void play()
+    {
+        game obj = new game(size);
+        while(!gameover)
+        {
+            if(player1){
+                System.out.println("Player1 , enter your X and Y position");
+                int x= sc.nextInt();
+                //System.out.println("Player1 , enter your Y position");
+                int y= sc.nextInt();
+                try{
+                    int res = obj.move(1, x, y);
+                    if(res != 0) {
+                        System.out.println("Player1 won the match");
+                        gameover = true ;
+                        break ;
+                    }
+                    player1 = false;
+                    player2 = true ;
+                }catch(IllegalArgumentException e){
+                    System.out.println(e);
+                    System.out.println("Player1 , enter values again ");
+                }
+            }
+            else{
+                System.out.println("Player2 , enter your X and Y position");
+                int x= sc.nextInt();
+                //System.out.println("Player2 , enter your Y position");
+                int y= sc.nextInt();
+                try{
+                    int res = obj.move(0, x, y);
+                    if(res != 0) {
+                        System.out.println("Player2 won the match");
+                        gameover = true ;
+                        break ;
+                    }
+                    player2 = false;
+                    player1 = true ;
+                }catch(IllegalArgumentException e){
+                    System.out.println(e);
+                    System.out.println("Player2 , enter values again ");
+                }
+            }
+        }
+    }
+}
